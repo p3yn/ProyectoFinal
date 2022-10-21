119473095588610113e4608374d8355de0793744
Ivan Medrano <jorge.medrano@cesunbc.edu.mx>
2022-10-20 22:28:48 -0700

    commit 2 / 20 oct

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -4,3 +4,10 @@ const salesCtrl = require ('../controllers/sales.controller')
 const verifyOps = require('../middlewares/authJwt')
 
 //RUTAS PARA VENTAS
+
+router.post('/add/', [verifyOps.verifyToken, verifyOps.isModerator], salesCtrl.createSales); //ocupa token
+router.get('/search/:salesId', salesCtrl.getSaleById);// cualquiera puede buscar  procutos por ID
+router.put('/update/:salesId', [verifyOps.verifyToken, verifyOps.isAdmin], salesCtrl.updateSaleById); //ocupa token
+router.delete('/delete/:salesId', [verifyOps.verifyToken, verifyOps.isAdmin], salesCtrl.deleteSaleById);//ocupa token
+
+module.exports = router;
